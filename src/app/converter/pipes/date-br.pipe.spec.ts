b4bf32fcf866f7596a69fa0b1ea273896f07c167
Williams Araujo <crfwilliams@gmail.com>
2020-07-03 09:44:49 +0100

    Implemented dateBr pipe
    Signed-off-by: Williams Araujo <crfwilliams@gmail.com>

--- src/app/converter/pipes/date-br.pipe.spec.ts
+++ src/app/converter/pipes/date-br.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('DateBrPipe', () => {
     const pipe = new DateBrPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('sould format date 2017-03-31 to 31/03/2017', () => {
+    const pipe = new DateBrPipe();
+    expect(pipe.transform('2017-03-31')).toEqual('31/03/2017');
+  });
 });
--- src/app/converter/pipes/date-br.pipe.ts
+++ src/app/converter/pipes/date-br.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateBrPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  /**
+   * Apply date formater
+   * 
+   * @param dateEn en date
+   * @return string date dd/mm/yyyy
+   */
+  transform(dateEn: string): string {
+    if (!dateEn) {
+      return '';
+    }
+
+    const dateArr = dateEn.split('-');
+
+    if (dateArr.length !== 3) {
+      return dateEn;
+    }
+
+    return dateArr[2] + '/' + dateArr[1] + '/' + dateArr[0];
   }
 
 }
